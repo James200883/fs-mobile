51cedd3b01c9423023ce1e6566132adb6974cdc9
xlh <james984557346@gmail.com>
2016-07-03 09:08:57 +0800

    frist commit

---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+
+})
+
+.controller('CategoryCtrl', function($scope, Chats) {
+  
+})
+
+.controller('AccountCtrl', function($scope, $stateParams, Chats) {
+  
+})
+
+.controller('BBSCtrl', function($scope) {
+  
+})
+.controller('CartCtrl', function ($scope) {
+    
+});
---
+++ www/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by xlh on 16/6/27.
+ */
+angular.module('starter.index', [])
+.factory('Index', function () {
+  var _homeUrl = '/fs-server/pageAds/findHomePageAds';
+  
+});
